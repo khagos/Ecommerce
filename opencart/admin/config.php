b04aa139fe772641051ffcbefdab05395a4b20c2
khagos.yytdevelopment@gmail.com <khagos.yytdevelopment@gmail.com>
2016-07-29 15:52:48 +0300

    initial commit

---
+++ opencart/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart/admin/');
+define('HTTP_CATALOG', 'http://localhost/opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencart/admin/');
+define('HTTPS_CATALOG', 'http://localhost/opencart/');
+
+// DIR
+define('DIR_APPLICATION', '/home/kibrom/Ecommerce/opencart/admin/');
+define('DIR_SYSTEM', '/home/kibrom/Ecommerce/opencart/system/');
+define('DIR_LANGUAGE', '/home/kibrom/Ecommerce/opencart/admin/language/');
+define('DIR_TEMPLATE', '/home/kibrom/Ecommerce/opencart/admin/view/template/');
+define('DIR_CONFIG', '/home/kibrom/Ecommerce/opencart/system/config/');
+define('DIR_IMAGE', '/home/kibrom/Ecommerce/opencart/image/');
+define('DIR_CACHE', '/home/kibrom/Ecommerce/opencart/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/kibrom/Ecommerce/opencart/system/storage/download/');
+define('DIR_LOGS', '/home/kibrom/Ecommerce/opencart/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/kibrom/Ecommerce/opencart/system/storage/modification/');
+define('DIR_UPLOAD', '/home/kibrom/Ecommerce/opencart/system/storage/upload/');
+define('DIR_CATALOG', '/home/kibrom/Ecommerce/opencart/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', '');
---
+++ opencart/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencart/');
+
+// DIR
+define('DIR_APPLICATION', '/home/kibrom/Ecommerce/opencart/catalog/');
+define('DIR_SYSTEM', '/home/kibrom/Ecommerce/opencart/system/');
+define('DIR_LANGUAGE', '/home/kibrom/Ecommerce/opencart/catalog/language/');
+define('DIR_TEMPLATE', '/home/kibrom/Ecommerce/opencart/catalog/view/theme/');
+define('DIR_CONFIG', '/home/kibrom/Ecommerce/opencart/system/config/');
+define('DIR_IMAGE', '/home/kibrom/Ecommerce/opencart/image/');
+define('DIR_CACHE', '/home/kibrom/Ecommerce/opencart/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/kibrom/Ecommerce/opencart/system/storage/download/');
+define('DIR_LOGS', '/home/kibrom/Ecommerce/opencart/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/kibrom/Ecommerce/opencart/system/storage/modification/');
+define('DIR_UPLOAD', '/home/kibrom/Ecommerce/opencart/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', '');
